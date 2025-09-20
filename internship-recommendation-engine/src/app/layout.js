04c5322697cb6164d6bd6f436fd42e78c297f4b4
Shubham shukla <shubhushukla586@gmail.com>
2025-09-20 16:22:05 +0530

    update

--- internship-recommendation-engine/src/app/layout.js
+++ internship-recommendation-engine/src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
         <ClientProviders>
           {children}
         </ClientProviders>
+        <Analytics />
       </body>
     </html>
   );
